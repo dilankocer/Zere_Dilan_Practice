84d492b2856dfff1c089a830a7cd721a64514ea5
zelemele2013 <zeraatmaca@gmail.com>
2021-10-08 15:09:45 -0400

    all sana commit

--- src/setup/First.java
+++ src/setup/First.java
@@ -3,5 +3,7 @@ package setup;
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello Zere");
+        System.out.println("Nene ne ne ne sen benden ne istiyoesun.");
     }
+
 }
